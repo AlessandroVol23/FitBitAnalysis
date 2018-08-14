8ad259a59c612e9fd5b2a1a6ef85c7a4399899bd
Sandro <alessandro.volpicella@gmail.com>
2018-08-14 12:37:52 +0200

    Linear Regression with multiple variables

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyse which factors have the biggest impact on the calorie consumption and trying to predict it for the day. ',
+    author='Sandro Volpicella',
+    license='',
+)
